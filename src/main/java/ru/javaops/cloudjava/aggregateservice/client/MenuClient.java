b2c850c0fafdbd7abdc6192483b0dfe69746b7f7
sserebryannikov <s.serebryannikovvv@gmail.com>
2025-11-26 12:14:24 +0300

    feat: implements client layer, and test

---
+++ src/main/java/ru/javaops/cloudjava/aggregateservice/client/MenuClient.java
@@ -0,0 +1,80 @@
+package ru.javaops.cloudjava.aggregateservice.client;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import ru.javaops.cloudjava.aggregateservice.config.props.ExternalServiceProps;
+import ru.javaops.cloudjava.aggregateservice.dto.aggregate.RatedMenuSort;
+import ru.javaops.cloudjava.aggregateservice.dto.menu.Category;
+import ru.javaops.cloudjava.aggregateservice.dto.menu.MenuItem;
+import ru.javaops.cloudjava.aggregateservice.exception.MenuAggregateException;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+
+import static ru.javaops.cloudjava.aggregateservice.dto.aggregate.RatedMenuSort.*;
+
+@Component
+public class MenuClient extends BaseClient {
+
+    private static final EnumSet<RatedMenuSort> SUPPORTED_SORTS = EnumSet.of(AZ, ZA, DATE_ASC, DATE_DESC, PRICE_ASC, PRICE_DESC);
+
+    private static final String MENU_BACKEND = "menuBackend";
+    private final WebClient webClient;
+
+    public MenuClient(WebClient.Builder clientBuilder,
+                      ExternalServiceProps props) {
+        super(props);
+        this.webClient = clientBuilder
+                .baseUrl(props.getMenuServiceUrl())
+                .build();
+    }
+
+    @CircuitBreaker(name = MENU_BACKEND)
+    @Retry(name = MENU_BACKEND)
+    public Mono<MenuItem> getMenuItem(Long menuId) {
+        var mono = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(props.getMenuItemPath())
+                        .path("/{menuId}")
+                        .build(menuId))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus(HttpStatusCode::is5xxServerError, response ->
+                        Mono.error(new MenuAggregateException("Menu Service unavailable", HttpStatus.SERVICE_UNAVAILABLE)))
+                .bodyToMono(MenuItem.class);
+
+        return applyTimeoutAndHandleExceptions(mono);
+    }
+
+    @CircuitBreaker(name = MENU_BACKEND)
+    @Retry(name = MENU_BACKEND)
+    public Mono<List<MenuItem>> getMenusForCategory(Category category, RatedMenuSort sort) {
+        var mono = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(props.getMenuItemPath())
+                        .queryParam("category", category)
+                        .queryParamIfPresent("sort",
+                                Optional.of(sort).filter(this::supported))
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus(HttpStatusCode::is5xxServerError, response ->
+                        Mono.error(new MenuAggregateException("Menu Service unavailable", HttpStatus.SERVICE_UNAVAILABLE)))
+                .bodyToMono(new ParameterizedTypeReference<List<MenuItem>>() {
+                });
+
+        return applyTimeoutAndHandleExceptions(mono);
+    }
+
+    private boolean supported(RatedMenuSort sort) {
+        return SUPPORTED_SORTS.contains(sort);
+    }
+}
---
+++ src/main/java/ru/javaops/cloudjava/aggregateservice/client/ReviewsClient.java
@@ -0,0 +1,76 @@
+package ru.javaops.cloudjava.aggregateservice.client;
+
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import ru.javaops.cloudjava.aggregateservice.config.props.ExternalServiceProps;
+import ru.javaops.cloudjava.aggregateservice.dto.review.GetRatingsRequest;
+import ru.javaops.cloudjava.aggregateservice.dto.review.RatedReviewsList;
+import ru.javaops.cloudjava.aggregateservice.dto.review.RatingsList;
+import ru.javaops.cloudjava.aggregateservice.dto.review.ReviewSort;
+import ru.javaops.cloudjava.aggregateservice.exception.MenuAggregateException;
+
+@Component
+public class ReviewsClient extends BaseClient {
+    private static final String REVIEW_BACKEND = "reviewBackend";
+    private final WebClient webClient;
+
+    public ReviewsClient(WebClient.Builder webclientBuilder,
+                         ExternalServiceProps props) {
+        super(props);
+        this.webClient = webclientBuilder
+                .baseUrl(props.getReviewServiceUrl())
+                .build();
+    }
+
+    @CircuitBreaker(name = REVIEW_BACKEND, fallbackMethod = "getReviewsWithMenuRatingCBFallback")
+    @Retry(name = REVIEW_BACKEND, fallbackMethod = "getReviewsWithMenuRatingRetryFallback")
+    public Mono<RatedReviewsList> getReviewsWithMenuRating(Long menuId, int from, int size, ReviewSort sort) {
+        var mono = webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(props.getMenuReviewsPath())
+                        .path("/{menuId}")
+                        .queryParam("from", from)
+                        .queryParam("size", size)
+                        .queryParam("sortBy", sort)
+                        .build(menuId))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus(HttpStatusCode::is5xxServerError, response ->
+                        Mono.error(new MenuAggregateException("Review Service unavailable", HttpStatus.SERVICE_UNAVAILABLE)))
+                .bodyToMono(RatedReviewsList.class);
+
+        return applyTimeoutAndHandleExceptions(mono);
+    }
+
+    @CircuitBreaker(name = REVIEW_BACKEND)
+    @Retry(name = REVIEW_BACKEND)
+    public Mono<RatingsList> getMenuRatings(GetRatingsRequest request) {
+        var mono = webClient
+                .post()
+                .uri(props.getMenuRatingsPath())
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+                .retrieve()
+                .onStatus(HttpStatusCode::is5xxServerError, response ->
+                        Mono.error(new MenuAggregateException("Review Service unavailable", HttpStatus.SERVICE_UNAVAILABLE)))
+                .bodyToMono(RatingsList.class);
+
+        return applyTimeoutAndHandleExceptions(mono);
+    }
+
+    private Mono<RatedReviewsList> getReviewsWithMenuRatingCBFallback(Long menuId, int from, int size, ReviewSort sort, CallNotPermittedException e) {
+        return Mono.just(RatedReviewsList.fallbackResponse(menuId));
+    }
+
+    private Mono<RatedReviewsList> getReviewsWithMenuRatingRetryFallback(Long menuId, int from, int size, ReviewSort sort, MenuAggregateException e) {
+        return Mono.just(RatedReviewsList.fallbackResponse(menuId));
+    }
+}
